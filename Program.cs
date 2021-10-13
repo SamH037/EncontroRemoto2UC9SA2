952b35937b4e9c759288f41e513dfb9701f4abb9
SamH037 <samuel.ign01@gmail.com>
2021-10-13 16:59:02 -0300

    adicionando atividade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cadastroPessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> texto = new List<string>();
+        }
+    }
+}
