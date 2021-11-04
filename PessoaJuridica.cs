d0f6533f72b5ab9db7180075ad8765f7a9f963f8
SamH037 <samuel.ign01@gmail.com>
2021-11-04 11:40:47 -0300

    atualizando para Encontro Remoto 5

--- PessoaJuridica.cs
+++ PessoaJuridica.cs
@@ -12,7 +12,7 @@ namespace cadastroPessoa
         // Validação se o cnpj virá com 14 digitos
         public bool ValidarCNPJ(string cnpj){
         // se quiser que avalie duas condições, deve-se colocar dois && (avaliar uma "&" outra)
-            if (cnpj.Length == 14 && cnpj.Substring(cnpj.Length - 4) == "0001") // substring retorna um pedaço do texto string original, o startIndex pega uma posição especifica da string (nesse caso, os 4 ultimos)
+            if (cnpj.Length == 14 && cnpj.Substring(cnpj.Length - 6, 4) == "0001") // substring retorna um pedaço do texto string original, o startIndex pega uma posição especifica da string (nesse caso, os 4 ultimos), o que queremos comparar é o 00001, então é necessario informar onde irá começar a comparação, e quantos numeros irá pegar, nesse caso, começar na 8º posição, e pegar 4 numeros a partir dali.
             { // o comando && só retorna verdadeiro se as duas condições forem cumpridas
                 return true;
             }
--- Program.cs
+++ Program.cs
@@ -1,67 +1,140 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
 
 namespace cadastroPessoa
 {
     class Program
     {
         static void Main(string[] args)
-        { // Cria-se um objeto para poder validar o outro objeto, um objeto recebe os valores, e um para chamar o método
-          //   PessoaFisica pFisica = new PessoaFisica();
+        {
+            string opcao;
 
-            //   PessoaFisica novapFisica = new PessoaFisica();
-            //   Endereco end = new Endereco();
-
-            //   end.logradouro = "Serra Azul, Q35";
-            //   end.numero = 1;
-            //   end.complemento = "Sobradinho/DF";
-            //   end.enderecoComercial = false;
-            // // repare que este recebeu os valores
-            //   novapFisica.endereco = end;
-            //   novapFisica.cpf = "123456789";
-            //   novapFisica.nome = "João";
-            //   novapFisica.dataNascimento = new DateTime(1992, 01, 25);
-            // // aqui acontece a consulta dos valores recebidos da pessoa cadastrada
-            //   Console.WriteLine($"Rua: {novapFisica.endereco.logradouro}, casa {novapFisica.endereco.numero}, {novapFisica.endereco.complemento}");
-            // // e aqui veja que o outro objeto está chamando a validação referente ao método
-            //   bool idadeValida = pFisica.ValidarDataNascimento(pFisica.dataNascimento);
-
-            //   if (idadeValida == true)
-            //   {
-
-            //       Console.WriteLine("Cadastro aprovado!");
-
-            //   }
-            //   else
-            //   {
-
-            //       Console.WriteLine("Cadastro reprovado, idade inválida!");
-
-            //   }  
-
-
-            PessoaJuridica pJuridica = new PessoaJuridica();
-
-            PessoaJuridica novaPjuridica = new PessoaJuridica();
-
-            Endereco end = new Endereco();
-
-            end.logradouro = "Q Central";
-            end.numero = 5;
-            end.complemento = "Banco do Brasil";
-            end.enderecoComercial = true;
-
-            novaPjuridica.endereco = end;
-            novaPjuridica.cnpj = "12345678900001";
-            novaPjuridica.RazaoSocial = "pessoajuridica";
-            // por padrão, quando se coloca um "if", ele já valida se é true. se colocar "!", ele valida se é falso
-            if (!pJuridica.ValidarCNPJ(novaPjuridica.cnpj))
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(@$"
+=================================================
+│       Bem vindo ao Sistema de cadastro de     │
+│           pessoas Fisica e Juridica           │
+=================================================
+");
+            // aqui chamamos a função de load que criamos fora da main no final do código, e atribuimos um valor para o texto que foi declarado na função original
+            LoadBar("Iniciando");
+            // o "do" e "while" são a estrutura de repetição, tudo que eu quero q se repita, estará dentro deste laço
+            do
             {
-                Console.WriteLine($"CNPJ Inválido");
-            }
-            else
+                Console.WriteLine(@$"
+=================================================
+│            Escolha uma das Opções abaixo      │
+│-----------------------------------------------│
+│           1 - Pessoa Fisica                   │
+│           2 - Pessoa Juridica                 │
+│                                               │
+│           0 - Sair                            │
+=================================================
+");     // aqui a opcao recebe o valor que o usuário escreveu
+                opcao = Console.ReadLine();
+                // switch case avalia as opções selecionadas pelo usuário
+                switch (opcao)
+                {
+                    case "1":
+                        // Cria-se um objeto para poder validar o outro objeto, um objeto recebe os valores, e um para chamar o método
+                        PessoaFisica pFisica = new PessoaFisica();
+
+                        PessoaFisica novapFisica = new PessoaFisica();
+
+                        Endereco endPf = new Endereco();
+
+                        endPf.logradouro = "Serra Azul, Q35";
+                        endPf.numero = 1;
+                        endPf.complemento = "Sobradinho/DF";
+                        endPf.enderecoComercial = false;
+                        // repare que este recebeu os valores
+                        novapFisica.endereco = endPf;
+                        novapFisica.cpf = "123456789";
+                        novapFisica.nome = "João";
+                        novapFisica.dataNascimento = new DateTime(1992, 01, 25);
+                        // aqui acontece a consulta dos valores recebidos da pessoa cadastrada
+                        Console.WriteLine($"Rua: {novapFisica.endereco.logradouro}, casa {novapFisica.endereco.numero}, {novapFisica.endereco.complemento}");
+                        // e aqui veja que o outro objeto está chamando a validação referente ao método
+                        bool idadeValida = pFisica.ValidarDataNascimento(pFisica.dataNascimento);
+
+                        if (idadeValida == true)
+                        {
+
+                            Console.WriteLine("Cadastro aprovado!");
+
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Cadastro reprovado, idade inválida!");
+
+                        }
+                        break;
+
+                    case "2":
+                        PessoaJuridica pJuridica = new PessoaJuridica();
+
+                        PessoaJuridica novaPjuridica = new PessoaJuridica();
+
+                        Endereco endPj = new Endereco();
+
+                        endPj.logradouro = "Q Central";
+                        endPj.numero = 5;
+                        endPj.complemento = "Banco do Brasil";
+                        endPj.enderecoComercial = true;
+
+                        novaPjuridica.endereco = endPj;
+                        novaPjuridica.cnpj = "34567890000199";
+                        novaPjuridica.RazaoSocial = "pessoajuridica";
+                        // por padrão, quando se coloca um "if", ele já valida se é true. se colocar "!", ele valida se é falso
+                        if (!pJuridica.ValidarCNPJ(novaPjuridica.cnpj))
+                        {
+                            Console.WriteLine($"CNPJ Inválido");
+                        }
+                        else
+                        {
+                            Console.WriteLine("CNPJ Válido");
+                        }
+                        break;
+
+                    case "0":
+
+                        Console.WriteLine($"Obrigado por utilizar nosso sistema");
+
+                        LoadBar("Finalizando");
+
+                        break;
+
+                    default:
+                        Console.WriteLine($"Opção inválida, por favor, digite uma opção válida");
+                        break;
+                }
+            } while (opcao != "0");
+            // while está avaliando a string opcao criada fora dele
+        }
+        // Função criada para barras de carregamento no código. Para que ela seja valida para todos os escopos, deve ser criada fora da main, e depois do escopo main, não antes
+        // para chamar uma função que não precisa ser instaciada, que é criada fora da main, necessita colocar o comando "static"
+        static void LoadBar(string textoLoad)
+        // o argumento criado "textoLoad" dentro do método LoadBar, passa o valor que vier nele para o método de carregamento criado, asism não precisarei criar um método para Iniciar e outro para Finalizar
+        {
+
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            // console write diferente de writeline não pula linha na hora de mostrar
+            Console.Write(textoLoad);
+            Thread.Sleep(300);
+            // Estrutura de repetição simples pelo laço for
+            for (var contador = 0; contador < 5; contador++)
             {
-                Console.WriteLine("CNPJ Válido");
+                Console.Write($".");
+                Thread.Sleep(250);
             }
+            Console.ResetColor();
+            // onde eu chamar o nome da função "loadbar", vai chamar esta função
         }
     }
 }
